bfb36f6d8bbaf51400589ad3ba5b2f77c1fd9824
RagnarokMew <ragnarokmewdev@gmail.com>
2026-06-21 23:40:22 +0300

    feat(hypr): add hypr env lua

---
+++ .config/hypr/env.lua
@@ -0,0 +1,11 @@
+local envs = {
+    MAIN_MONITOR_PORT = "eDP-1", -- Change to your own
+    PROJECTOR_PORT = "HDMI-1",   -- Change to your own
+    XCURSOR_SIZE = "24",
+    HYPRCURSOR_SIZE = "24",
+    QT_QPA_PLATFORMTHEME = "qt6ct",
+}
+
+for k, v in envs do
+    hl.env(k, v)
+end
